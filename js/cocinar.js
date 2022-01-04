e1f7b3cd743df90a6ed4951d84cc6d8ff4517efd
Hdoc <72316111+Hdoc1509@users.noreply.github.com>
2022-01-04 16:51:14 -0500

    Add files via upload

--- js/cocinar.js
+++ js/cocinar.js
@@ -1,7 +1,5 @@
 //@ts-check
 
-import { arrayRandElement, cleanArray } from "./libs/my-array";
-
 /**
  * Array of foods
  * @type {Array<String>}
@@ -43,6 +41,25 @@ const foodsToCook = [
  * @type {Array<String>}
  */
 const generatedFoods = [];
+
+/**
+ * Get a random element from an array
+ * @param {Array} array
+ * @returns {Number | String | Array | Object}
+ */
+function arrayRandElement(array) {
+	let index = Math.floor(Math.random() * array.length);
+	let randElement = array[index]
+	return randElement;
+}
+
+/**
+ * Clean all elements of an array
+ * @param {Array} array
+ */
+function cleanArray(array) {
+	array.splice(0, array.length);
+}
 
 /**
  * Generate an unique food
