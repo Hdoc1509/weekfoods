2f3cf1784d971483ec9dc1dc58c951ab6a9b65e0
Hdoc <72316111+Hdoc1509@users.noreply.github.com>
2022-01-06 11:43:18 -0500

    Update cocinar.js

--- js/cocinar.js
+++ js/cocinar.js
@@ -45,25 +45,6 @@ const foodsToCook = [
 const generatedFoods = [];
 
 /**
- * Get a random element from an array
- * @param {Array} array
- * @returns {Number | String | Array | Object}
- */
-/*function arrayRandElement(array) {
-	let index = Math.floor(Math.random() * array.length);
-	let randElement = array[index]
-	return randElement;
-}*/
-
-/**
- * Clean all elements of an array
- * @param {Array} array
- */
-/*function cleanArray(array) {
-	array.splice(0, array.length);
-}*/
-
-/**
  * Generate an unique food
  */
 function generateUniqueFood() {
