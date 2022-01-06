79b3bb277e95a4aae6c09c6a6f79479c75cfe54e
Hdoc <72316111+Hdoc1509@users.noreply.github.com>
2022-01-06 13:26:54 -0500

    Add files via upload

--- js/modules/my-array.js
+++ js/modules/my-array.js
@@ -3,14 +3,23 @@
  * @param {Array} array
  */
 function improveArray(array) {
-   array.randElement = function () {
-      let index = Math.floor(Math.random() * this.length);
-      return this[index];
-   };
+   if (Array.isArray(array)) {
+      array.randElement = function () {
+         let index = Math.floor(Math.random() * this.length);
+         return this[index];
+      };
 
-   array.clean = function () {
-      this.splice(0, this.length);
-   };
+      array.clean = function () {
+         this.splice(0, this.length);
+      };
+
+      array.removeElement = function (element) {
+         let elementPosition = this.indexOf(element);
+         this.splice(elementPosition, 1);
+      };
+   } else {
+      console.error("Uncaught TypeError: 'improveArray' function only allows arrays as parameter");
+   }
 }
 
 export { improveArray };
