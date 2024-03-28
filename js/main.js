f560628b64a3347747b47008a0c62611249df466
hdoc <hector.ochoa.dev@gmail.com>
2024-03-28 12:10:18 -0500

    style: format files

--- js/main.js
+++ js/main.js
@@ -7,40 +7,40 @@ import { improveArray } from "./modules/my-array.js";
  * @type {Array<String>}
  */
 const foodsToCook = [
-	"Escabeche de pollo",
-	"Pollo al sillao",
-	"Estofado de pollo",
-	"Ají de pollo",
-	"Lomo saltado",
-	"Carapulcra",
-	"Puré de papas",
-	"Arroz con pollo",
-	"Seco de carne",
-	"Arroz a la jardinera",
-	"Cau-cau",
-	"Arroz con lentejas",
-	"Tallarín verde",
-	"Tallarín rojo",
-	"Macarrones",
-	"Olluco con carne",
-	"Picante de carne",
-	"Arroz chaufa",
-	"Bistek con papa sancochada",
-	"Pollo a la olla",
-	"Pollo broaster",
-	"Pollo al horno",
-	"Causa",
-	"Chanfainita",
-	"Arroz con huevo",
-	"Papa a la huancaína",
-	"Ensalada rusa con milanesa",
-	"Arroz con atún",
-	"Alverjita verde"
+  "Escabeche de pollo",
+  "Pollo al sillao",
+  "Estofado de pollo",
+  "Ají de pollo",
+  "Lomo saltado",
+  "Carapulcra",
+  "Puré de papas",
+  "Arroz con pollo",
+  "Seco de carne",
+  "Arroz a la jardinera",
+  "Cau-cau",
+  "Arroz con lentejas",
+  "Tallarín verde",
+  "Tallarín rojo",
+  "Macarrones",
+  "Olluco con carne",
+  "Picante de carne",
+  "Arroz chaufa",
+  "Bistek con papa sancochada",
+  "Pollo a la olla",
+  "Pollo broaster",
+  "Pollo al horno",
+  "Causa",
+  "Chanfainita",
+  "Arroz con huevo",
+  "Papa a la huancaína",
+  "Ensalada rusa con milanesa",
+  "Arroz con atún",
+  "Alverjita verde",
 ];
 
 improveArray(foodsToCook);
 
-/** 
+/**
  * Array for generated foods
  * @type {Array<String>}
  */
@@ -52,21 +52,21 @@ improveArray(generatedFoods);
  * Generate an unique food
  */
 function generateUniqueFood() {
-	let food = foodsToCook.randElement();
+  let food = foodsToCook.randElement();
 
-	while (generatedFoods.includes(food)) {
-		food = foodsToCook.randElement();
-	}
+  while (generatedFoods.includes(food)) {
+    food = foodsToCook.randElement();
+  }
 
-	generatedFoods.push(food);
+  generatedFoods.push(food);
 }
 
-document.getElementById("botoncito").addEventListener("click",function() {
-	// Generating and adding the food to the corresponding cell
-	for (let i = 0; i < 5; i++) {
-		generateUniqueFood();
-		document.getElementById(`food${i + 1}`).innerText = generatedFoods[i];
-	}
+document.getElementById("botoncito").addEventListener("click", function () {
+  // Generating and adding the food to the corresponding cell
+  for (let i = 0; i < 5; i++) {
+    generateUniqueFood();
+    document.getElementById(`food${i + 1}`).innerText = generatedFoods[i];
+  }
 
-	generatedFoods.clean();
+  generatedFoods.clean();
 });
